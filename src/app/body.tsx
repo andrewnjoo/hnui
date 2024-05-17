69ddfc1c8b7bc0c59815c549605f0cc23b3d1dfe
Andrew Njoo <adnjoo@gmail.com>
2024-05-16 19:48:56 -0700

    dark

--- src/app/body.tsx
+++ src/app/body.tsx
@@ -49,7 +49,7 @@ export const Body = () => {
         <div className='lg:w-[800px]'>
           <Table>
             <TableBody>
-              <img src='/y18.svg' alt='y18' className='w-4 h-4' />
+              <img src='/y18.svg' alt='y18' className='w-4 h-4 border border-white border-[0.5px]' />
               {data &&
                 data?.map((story: Story) => (
                   <TableRow key={story.id}>
