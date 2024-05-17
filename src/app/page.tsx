0c0060c61e2eea57568cedbb8d91aae705bcd5e0
Andrew Njoo <adnjoo@gmail.com>
2024-05-16 20:28:35 -0700

    responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Body } from './body';
 
 export default async function Home() {
   return (
-    <main className='flex flex-col p-12'>
+    <main className='flex flex-col sm:p-12'>
       <Body />
     </main>
   );
