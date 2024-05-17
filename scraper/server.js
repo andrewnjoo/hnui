1551cdce007e681c2beaee774847e227232fd6df
Andrew Njoo <adnjoo@gmail.com>
2024-05-16 17:29:33 -0700

    route

--- scraper/server.js
+++ scraper/server.js
@@ -1,9 +1,46 @@
 const express = require("express");
 const cors = require("cors");
-const scrapeHackerNews = require("./scraper");
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+async function scrapeHackerNews(page = 1) {
+  const url = "https://news.ycombinator.com/?p=" + page;
+  const response = await fetch(url);
+  const html = await response.text();
+
+  const $ = cheerio.load(html);
+  const stories = [];
+
+  $("tr.athing").each((index, element) => {
+    const id = $(element).attr("id");
+    const title = $(element).find(".titleline > a").text();
+    const link = $(element).find(".titleline > a").attr("href");
+    const points = $(element)
+      .next()
+      .find(".score")
+      .text()
+      .replace("points", "");
+    let comments = $(element)
+      .next()
+      .find('a[href^="item"]')
+      .last()
+      .text()
+      .replace("comments", "")
+      .replace("comment", "")
+      .replace("&nbsp;", "");
+
+    if (comments === "discuss") {
+      comments = "0";
+    }
+
+    stories.push({ id, title, link, points, comments });
+  });
+
+  return stories;
+}
 
 app.use(cors());
 
