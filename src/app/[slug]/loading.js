3b1b1ad66aa73c5fb4fe12bcbbbaca6bed890b64
Andrew Njoo <adnjoo@gmail.com>
2024-05-17 19:50:00 -0700

    loading

---
+++ src/app/[slug]/loading.js
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className='mt-12'>
+      <div className='mx-auto mt-12 flex justify-center items-center text-french-gray'>
+        Loading...
+      </div>
+    </div>
+  );
+}
