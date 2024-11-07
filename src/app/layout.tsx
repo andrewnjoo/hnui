950c09924e0c5a25f312dde117a503bb5b67a4ca
Andrew Njoo <adnjoo@gmail.com>
2024-11-07 17:33:32 -0600

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 import { Footer } from '@/components/Footer';
 import './globals.css';
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
